631608a6c3c36d94102be494ab13b491b970442e
vaaPo <ripraptiptop@gmail.com>
2026-06-14 12:55:16 +0300

    Format sample SQL example

--- examples/select_from_dual_demo.sql
+++ examples/select_from_dual_demo.sql
@@ -1,5 +1,7 @@
 -- v2
 -- author: OpenCode / gpt-5.4
--- timestamp: 2026-06-14T10:51:08+0300
+-- timestamp: 202606141051
 -- Fixed Trivadis warning G-3145 by replacing SELECT * with an explicit column list.
-select dummy from dual;
+select
+    dummy
+from dual;
